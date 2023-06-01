7f1e2f0e07ae50ebe9926095f2d4763d09036452
casey <21181214451@stu.xidian.edu.cn>
2023-06-01 16:20:13 +0800

    完善客户连接类HttpConnection

--- src/http/http_connection.cpp
+++ src/http/http_connection.cpp
@@ -1,14 +1,12 @@
 #include "http_connection.h"
 
-int HttpConnection::m_http_connection_numner = 0;
-bool HttpConnection::is_ET_mode;
-std::string HttpConnection::m_resource_dir;
+std::atomic<int> HttpConnection::http_connection_numner_;
+std::string HttpConnection::resource_dir_;
+bool HttpConnection::is_ET_mode_;
 
-HttpConnection::HttpConnection() 
+HttpConnection::HttpConnection() : fd_(-1), is_close_(true), iov_num_(0) 
 { 
-    m_fd = -1;
-    m_address = { 0 };
-    is_close = true;
+    address_ = { 0 };
 };
 
 HttpConnection::~HttpConnection() 
@@ -19,54 +17,107 @@ HttpConnection::~HttpConnection()
 void HttpConnection::Initialization(int fd, const sockaddr_in& addr) 
 {
     assert(fd > 0);
-    m_http_connection_numner++;
-    m_address = addr;
-    m_fd = fd;
-    is_close = false;
+    fd_ = fd;
+    address_ = addr;
+    is_close_ = false;
+    read_buf_.RetrieveAll();
+    write_buf_.RetrieveAll();
+    http_connection_numner_++;
+    // 打印日志
 }
 
-void HttpConnection::Close() 
+ssize_t HttpConnection::Read(int &error_num)
 {
-    if(is_close == false)
+    ssize_t len = -1;
+    // 若为 ET 模式的话，需要一直读直到所有数据读完
+    do
     {
-        is_close = true; 
-        m_http_connection_numner--;
-        close(m_fd);
-    }
-}
-
-int HttpConnection::GetFd() const 
-{
-    return m_fd;
-};
-
-struct sockaddr_in HttpConnection::GetAddress() const 
-{
-    return m_address;
-}
-
-int HttpConnection::Read(int &error_num) {
-    int len = -1;
-    return len;
-}
-
-int HttpConnection::Write(int &error_num) 
-{
-    int len = -1;
+        len = read_buf_.ReadFd(fd_, error_num);
+        if (len <= 0)
+        {
+            break;
+        }
+    } while (is_ET_mode_);
     return len;
 }
 
 bool HttpConnection::Process() 
 {
+    request_.Initialization();
+    if (read_buf_.ReadableBytes() <= 0)
+    {
+        return false;
+    }
+    else if (request_.Parse(read_buf_))
+    {
+        // 打印日志
+        response_.Initialization(resource_dir_, request_.GetPath(), request_.GetIsKeepAlive(), 200);
+    }
+    else
+    {
+        response_.Initialization(resource_dir_, request_.GetPath(), false, 400);
+    }
+    response_.MakeResponse(write_buf_);
+    // 响应头
+    iov_[0].iov_base = write_buf_.GetReadPtr();
+    iov_[0].iov_len = write_buf_.ReadableBytes();
+    iov_num_ = 1;
+    // 请求文件
+    if (response_.GetFileSize() > 0 && response_.File())
+    {
+        iov_[1].iov_base = response_.File();
+        iov_[1].iov_len = response_.GetFileSize();
+        iov_num_ = 2;
+    }
+    // 打印日志
     return true;
 }
 
-bool HttpConnection::IsKeepAlive() const
+ssize_t HttpConnection::Write(int &error_num)
 {
-    return is_close;
+    ssize_t len = -1;
+    do
+    {
+        len = writev(fd_, iov_, iov_num_);
+        if (len <= 0)
+        {
+            error_num = errno;
+            break;
+        }
+        if (GetToWriteBytes() == 0)
+        {
+            break; // 传输完成
+        }
+        // 第一块内存缓冲区中的数据写完了，清空第一块缓冲区，更新第二块缓冲区的起始位置和大小
+        // 注意 iov_[1].iov_base 是 void *类型的，对无类型指针进行算术运算是不被允许的，因为编译器无法确定运算的单位大小
+        else if (static_cast<size_t>(len) > iov_[0].iov_len)
+        {
+            iov_[1].iov_base = static_cast<char *>(iov_[1].iov_base) + (len - iov_[0].iov_len);
+            iov_[1].iov_len -=  (len - iov_[0].iov_len);
+            if (iov_[0].iov_len)
+            {
+                write_buf_.RetrieveAll();
+                iov_[0].iov_len = 0;
+            }
+        }
+        else // 第一块缓冲区中的数据还未写完，仅更新第一块缓冲区的相关信息
+        {
+            iov_[0].iov_base = static_cast<char *>(iov_[0].iov_base) + len;
+            iov_[0].iov_len -= len;
+            write_buf_.Retrieve(len);
+        }
+    } while (is_ET_mode_ || GetToWriteBytes() > 0);
+    return len;   
 }
 
-int HttpConnection::GetToWriteBytes() const
+void HttpConnection::Close() 
 {
-    return m_fd;
+    response_.UnmapFile();
+    if(is_close_ == false)
+    {
+        is_close_ = true; 
+        http_connection_numner_--;
+        close(fd_);
+        // 打印日志
+    }
 }
--- src/http/http_connection.h
+++ src/http/http_connection.h
@@ -5,36 +5,90 @@
 #include <cassert>
 #include <unistd.h>
 #include <arpa/inet.h>
-
+#include <sys/uio.h> // struct iovec
+#include <atomic>
+#include "../buffer/buffer.h"
+#include "http_request.h"
+#include "http_response.h"
 
 class HttpConnection
 {
 public:
+
+    static std::atomic<int> http_connection_numner_;    // 保存连接的客户端数目，所有实例共享，定义为原子操作
+
+    static std::string resource_dir_;   // 保存资源的存放地址
+
+    static bool is_ET_mode_;            // 保存客户的连接是否为 ET 模式
+
     HttpConnection();
+
     ~HttpConnection();
+
+    // 初始化一个连接
     void Initialization(int fd, const struct sockaddr_in &addr);
 
-    int GetFd() const;
-    struct sockaddr_in GetAddress() const;
-    void Close();
+    // 读请求报文
+    ssize_t Read(int &error_num);
 
-    int Read(int &error_num);
-
-    int Write(int &error_num);
-
-    int GetToWriteBytes() const;
-
-    bool IsKeepAlive() const;
-
+    // 让 request_ 解析读到的请求报文并让 response_ 生成响应报文，设置内存缓冲区 iovec 的信息
     bool Process();
 
-    static int m_http_connection_numner;
-    static std::string m_resource_dir;
-    static bool is_ET_mode;
-private:
-    int m_fd;
-    struct sockaddr_in m_address;
-    bool is_close;
-};
+    // 将生成的响应报文写回
+    ssize_t Write(int &error_num);
 
+    // 关闭连接
+    void Close();
+
+    int GetToWriteBytes() const
+    {
+        return iov_[0].iov_len + iov_[1].iov_len;
+    }
+
+    int GetFd() const
+    {
+        return fd_;
+    }
+
+    int GetPort() const
+    {
+        return ntohs(address_.sin_port);
+    }
+
+    const char *GetIP() const
+    {
+        char ip[16] = {};
+        return inet_ntop(AF_INET, &address_.sin_addr.s_addr, ip, 16);
+    }
+    
+    struct sockaddr_in GetAddress() const
+    {
+        return address_;
+    }
+
+    // 返回请求报文中的 Connection 信息
+    bool IsKeepAlive() const
+    {
+        return request_.GetIsKeepAlive();
+    }
+
+private:
+    int fd_;            // 客户端连接的 fd
+
+    struct sockaddr_in address_; // 客户端地址
+
+    bool is_close_;     // 当前连接是否关闭
+
+    int iov_num_;
+
+    struct iovec iov_[2];
+
+    Buffer read_buf_;   // 读缓冲区
+
+    Buffer write_buf_;  // 写缓冲区
+
+    HttpRequest request_;
+
+    HttpResponse response_;
+};
 #endif
